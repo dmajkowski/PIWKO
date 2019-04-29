61b577b3ebfd0f0a3816833c1daaf1a06c999e6a
dmajkowski <dmajkowski@onet.pl>
2019-04-29 23:54:54 +0200

    Sign in and Sin on rdy - needs some work

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,6 +1,43 @@
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      document.getElementById("login_div").style.display = "none"
+      document.getElementById("loggedin_div").style.display = "flex";
+    } else {
+      // No user is signed in.
+
+    }
+  });
+
+
 function login(){
 
     var userEmail = document.getElementById("email_field").value;
     var userPass = document.getElementById("password_field").value;
 
+
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        window.alert("Error :" + errorMessage);
+
+        // ...
+      });
+
+}
+
+function signup(){
+
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
+
+    firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      });
 }

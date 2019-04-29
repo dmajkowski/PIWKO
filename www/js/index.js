38218b8b9dde7a65ff926a424c6d36c88b404adc
dmajkowski <dmajkowski@onet.pl>
2019-04-29 23:12:56 +0200

    Added buttons

--- www/js/index.js
+++ www/js/index.js
@@ -1,33 +1,6 @@
-(function(){
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyCJUe6lJGDiEOg0ZzkVDOutlUjtTOZk0hA",
-  authDomain: "piwko-4c891.firebaseapp.com",
-  databaseURL: "https://piwko-4c891.firebaseio.com",
-  projectId: "piwko-4c891",
-  storageBucket: "piwko-4c891.appspot.com",
-  messagingSenderId: "1039773129892"
-};
-firebase.initializeApp(config);
+function login(){
 
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
 
-const txtEmail = document.getElementById('txtEmail');
-const txtPassword = document.getElementById('txtPassword');
-const btnLogin = document.getElementById('btnLogin');
-const btnSignUp = document.getElementById('btnSignUp');
-const btnLogout = document.getElementById('btnLogout');
-
-
-btnLogin.addEventListener('click', e => {
-
-    const email = txtEmail.value;
-    const pass = txtPassword.value;
-    const auth = firebase.auth();
-
-    const promise = auth.signInWithEmailAndPassword(email, pass);
-
-    promise.catch(e => console.log(e.message));
-
-});
-
-}());
+}

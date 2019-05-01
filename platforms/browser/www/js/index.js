c02724c94b39ff4627eae5531cb0c0a5a8f8d215
dmajkowski <dmajkowski@onet.pl>
2019-05-01 08:50:31 +0200

    Firebase working

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,17 +1,19 @@
 
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-      document.getElementById("login_div").style.display = "none"
-      document.getElementById("loggedin_div").style.display = "flex";
-    } else {
-      // No user is signed in.
-      document.getElementById("login_div").style.display = "flex"
-      document.getElementById("loggedin_div").style.display = "none";
-    }
-  });
+//Dodawanie nowego użytkownika
+function signup(){
 
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
 
+    firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      });
+}
+
+//Logowanie sie za pomoca maila i hasla
 function login(){
 
     var userEmail = document.getElementById("email_field").value;
@@ -30,15 +32,31 @@ function login(){
 
 }
 
-function signup(){
 
-    var userEmail = document.getElementById("email_field").value;
-    var userPass = document.getElementById("password_field").value;
+//Sprawdzenie Czy uzytkownik zalogowany
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      document.getElementById("login_div").style.display = "none"
+      document.getElementById("loggedin_div").style.display = "flex";
+      document.getElementById("loggedout_div").style.display = "none";
+    } else {
+      // No user is signed in.
+      document.getElementById("login_div").style.display = "flex"
+      document.getElementById("loggedin_div").style.display = "none";
+      document.getElementById("loggedout_div").style.display = "none";
+    }
+  });
 
-    firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-      });
+//Wylogowanie
+function logout(){
+firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+    document.getElementById("login_div").style.display = "none"
+    document.getElementById("loggedin_div").style.display = "none";
+    document.getElementById("loggedout_div").style.display = "flex";
+  }).catch(function(error) {
+    // An error happened.
+  });
 }
+

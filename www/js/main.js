489fa2607f9e2d557578980318c79f33c41c4d7f
unknown <dmajkowski@onet.pl>
2019-05-10 23:13:15 +0200

    Camera errors fixing

--- www/js/main.js
+++ www/js/main.js
@@ -23,6 +23,30 @@ function logout(){
        //skanowanie kodu
        let kod = "";
        function scan(){
+
+        if (navigator.getUserMedia) {
+  // Request the camera.
+  navigator.getUserMedia(
+    // Constraints
+    {
+      video: true
+    },
+
+    // Success Callback
+    function(localMediaStream) {
+
+    },
+
+    // Error Callback
+    function(err) {
+      // Log the error to the console.
+      console.log('The following error occurred when trying to use getUserMedia: ' + err);
+    }
+  );
+
+} else {
+  alert('Sorry, your browser does not support getUserMedia');
+}
       //https://www.dynamsoft.com/CustomerPortal/Portal/TrialLicense.aspx
          BarcodeReader.licenseKey = 't0068MgAAAAxT9peWqAbLNI2gDlg9yk8dqzhp5Me5BNCgFIg2p5X+8TPYghCr9cz6TNFlkmkpzOJelNHJaQMWGe7Bszoxoo4=';
          let scanner = new BarcodeReader.Scanner({

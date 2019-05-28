85592b201960e94e25b8f582e3525eeefd0f3b80
180866 <marcin.leganowski@gmail.com>
2019-05-29 00:51:12 +0200

    checking api capabilities

--- www/js/main.js
+++ www/js/main.js
@@ -48,3 +48,44 @@ console.log('main.html');
 console.log('User:   ' + email_uzytkownika);
 console.log('KOD: ' + kod);
 
+
+////API///
+
+// var request = new XMLHttpRequest()
+
+// request.open('GET', 'https://world.openfoodfacts.org/api/v0/product/737628064502.json', true)
+// request.onload = function() {
+//   // Begin accessing JSON data here
+//   var data = JSON.parse(this.response)
+
+//   if (request.status >= 200 && request.status < 400) {
+//     data.forEach(product => {
+//       console.log(product.code)
+//     })
+//   } else {
+//     console.log('error')
+//   }
+// }
+
+// request.send()
+
+
+
+// const ul = document.getElementById('authors');
+const url = 'https://world.openfoodfacts.org/product/5901559140009';
+// fetch(url)
+//   .then(function(data) {
+//     // Here you get the data to modify as you please
+//     let kodzik = data.json();
+//     console.log(kodzik);
+//     })
+//   .catch(function(error) {
+//     // If there is any error you will catch them here
+//   });   
+let skrap = '';
+fetch(url)
+    .then(response => response.json())
+    .then(json => {console.log('wczesniej: ' + json.code), skrap = json.code})
+    .then(json => console.log("to" + skrap));
+
+
